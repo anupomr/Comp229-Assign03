b669313e2b8f8261f4201d54a8096c2a44176578
Anupom Roy <anupomr@yahoo.com>
2017-11-14 16:24:18 -0500

    Added course Deatails

--- Comp229-Assign03/Courses.aspx
+++ Comp229-Assign03/Courses.aspx
@@ -1,3 +1,19 @@
 ﻿<%@ Page Title="Courses" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Courses.aspx.cs" Inherits="Comp229_Assign03.Courses" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="CourseDetails" runat="server" AutoGenerateColumns="false"
+         CssClass="table table-bordered table-striped table-hover" OnRowDeleting="CourseDetails_RowDeleting"
+         DataKeyNames="EnrollmentID">
+            <Columns>
+                <asp:BoundField DataField="LastName" HeaderText="Last Name" Visible="true" />
+                <asp:BoundField DataField="FirstMidName" HeaderText="First Name" Visible="true" />
+                <asp:CommandField DeleteText="<i class='fa fa-trash-o fa-lg'></i> Delete" 
+                     ShowDeleteButton="true" ButtonType="Link" ControlStyle-CssClass="btn btn-danger btn-sm" />
+
+            </Columns>
+
+
+    </asp:GridView>
+
+    <a class="btn btn-primary btn-sm" href="Default.aspx"> Home</a>
+
 </asp:Content>
--- Comp229-Assign03/Courses.aspx.cs
+++ Comp229-Assign03/Courses.aspx.cs
@@ -5,12 +5,37 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Comp229_Assign03.Models;
+
+
 namespace Comp229_Assign03
 {
     public partial class Courses : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.GetStudents();
+        }
+
+        protected void CourseDetails_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+        private void GetStudents()
+        {
+            int CourseID = Convert.ToInt32(Request.QueryString["CourseID"]);
+            //
+            using (ControlsoContext db = new ControlsoContext())
+            {
+                // query the Student table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                join enrollment in db.Enrollments
+                                on allStudents.StudentID equals enrollment.StudentID
+                                where enrollment.CourseID == CourseID
+                                select new { LastName = allStudents.LastName, FirstMidName = allStudents.FirstMidName, EnrollmentID = enrollment.EnrollmentID });
+                CourseDetails.DataSource = Students.ToList();
+                CourseDetails.DataBind();
+            }
 
         }
     }
